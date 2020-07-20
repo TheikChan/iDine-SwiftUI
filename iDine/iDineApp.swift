89993efaddef921bbe6661b89a802a1f69888d28
TheikChan <theikchan.tc@gmail.com>
2020-07-20 11:02:25 +0630

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by DaewiChan on 7/20/20.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
